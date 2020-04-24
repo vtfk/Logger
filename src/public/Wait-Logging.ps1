6a5c8c05ea66f98ccc1601719c380d8ac2630095
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2020-04-24 11:35:26 +0200

    Fixed output...

--- src/public/Wait-Logging.ps1
+++ src/public/Wait-Logging.ps1
@@ -2,9 +2,6 @@ Function Wait-Logging
 {
     [CmdletBinding()]
     param()
-
-    $ErrorMsg = "Cmdlet `"Wait-Logging`" is deprecated and should be removed!"
     
-    Write-Log $ErrorMsg -Level WARNING
-    Write-Warning $ErrorMsg
+    Write-Warning 'Cmdlet "Wait-Logging" is deprecated and should be removed!'
 }
