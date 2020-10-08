619a42da76db361e3d153c551e949c88b1647185
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2020-10-08 09:56:21 +0200

    Bugfix: When CMTrace and File are given they will overwrite each other. Now the user at least will be aware of it

--- src/public/Add-LogTarget.ps1
+++ src/public/Add-LogTarget.ps1
@@ -37,6 +37,14 @@ function Add-LogTarget
     {
         $Script:Logging.EnabledTargets[$PSBoundParameters.Name] = Merge-DefaultConfig -Target $PSBoundParameters.Name -Configuration $Configuration
 
+        if ($Script:Logging.EnabledTargets.ContainsKey('CMTrace') -and $Script:Logging.EnabledTargets.ContainsKey('File') -and (($null -eq $Script:Logging.EnabledTargets['CMTrace'].Path -and $null -eq $Script:Logging.EnabledTargets['File'].Path) -or ($null -ne $Script:Logging.EnabledTargets['CMTrace'].Path -and $null -ne $Script:Logging.EnabledTargets['File'].Path -and $Script:Logging.EnabledTargets['CMTrace'].Path -eq $Script:Logging.EnabledTargets['File'].Path))) {
+            # both CMTrace and File has been given
+                # and Path on both of them are null 
+                # or Path is given on both but they are equal...
+            
+            Write-Warning "'CMTrace' and 'File' logs will have the same name and will overwrite each other, unless you specify a unique Path on one of them"
+        }
+
         if ($Script:Logging.EnabledTargets[$PSBoundParameters.Name].Init -is [scriptblock])
         {
             & $Script:Logging.EnabledTargets[$PSBoundParameters.Name].Init $Configuration
